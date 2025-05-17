a4bf317652445b933025f7719cfec09c656f0bd1
Abhay Malakar <malakarabhay1@gmail.com>
2025-05-17 23:33:47 +0530

    ab

--- routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { makePayment } = require('../controllers/paymentController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.post('/', authMiddleware, makePayment);
-
-module.exports = router;
